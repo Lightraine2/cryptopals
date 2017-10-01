d7f7d34531be0129222a2a5b11b5fb455e56249b
Lightraine2 <laurencedale1986@gmail.com>
2017-10-01 21:19:43 +0100

    Update C1S1solution.py

--- C1S1solution.py
+++ C1S1solution.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python
 
+import binascii
 import base64
 
-string = input("Input string to encode...")
+def hexToBase64(s):
+    decoded = binascii.unhexlify(s)
+    return base64.b64encode(decoded).decode('ascii')
 
-output = base64.b64encode(bytes('string', 'utf-8'))
-
-print output
+x = raw_input("Input your hex encoded string here:")
+print "Taking hex encoded string and outputting to base64 encoded"
+y = hexToBase64(x)
+print "your Base64 encoded string is:"
+print(y)
